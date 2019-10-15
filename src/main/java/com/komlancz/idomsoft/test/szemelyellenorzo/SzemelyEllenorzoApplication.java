5b22fd0f9795e45ccf07984d0cde4395154c1804
Komlancz Janos <jkomlancz@gmail.com>
2019-10-15 18:23:37 +0200

    initial commit

---
+++ src/main/java/com/komlancz/idomsoft/test/szemelyellenorzo/SzemelyEllenorzoApplication.java
@@ -0,0 +1,13 @@
+package com.komlancz.idomsoft.test.szemelyellenorzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzemelyEllenorzoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzemelyEllenorzoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/komlancz/idomsoft/test/szemelyellenorzo/SzemelyEllenorzoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.komlancz.idomsoft.test.szemelyellenorzo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SzemelyEllenorzoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
