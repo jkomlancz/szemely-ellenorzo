e33d3003e04fd151e7a8c34dbfb58c52712888d3
Komlancz Janos <jkomlancz@gmail.com>
2019-12-08 20:08:46 +0100

    use ribbon LB for access servers

--- src/main/java/com/komlancz/idomsoft/test/szemelyellenorzo/service/okmanyellenorzo/OkmanyEllenorzoServiceProxy.java
+++ src/main/java/com/komlancz/idomsoft/test/szemelyellenorzo/service/okmanyellenorzo/OkmanyEllenorzoServiceProxy.java
@@ -2,13 +2,16 @@ package com.komlancz.idomsoft.test.szemelyellenorzo.service.okmanyellenorzo;
 
 import com.komlancz.idomsoft.test.szemelyellenorzo.model.OkmanyDTO;
 import com.komlancz.idomsoft.test.szemelyellenorzo.service.okmanyellenorzo.model.OkmanyEllenorzoValasz;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name = "okmany-ellenorzo-service", url = "localhost:5502")
+//@FeignClient(name = "okmany-ellenorzo-service", url = "localhost:5502")
+@FeignClient(name = "okmany-ellenorzo-service")
+@RibbonClient(name = "okmany-ellenorzo-service")
 public interface OkmanyEllenorzoServiceProxy {
 
     @PostMapping(path = "/okmany-ellenorzes")
